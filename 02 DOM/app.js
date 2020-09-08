5981267db778fadd69dff8d45413f137a466accf
Souka1987 <soukainataattoumani@yahoo.fr>
2020-09-08 16:56:20 +0200

    08/09

---
+++ 02 DOM/app.js
@@ -0,0 +1,51 @@
+// DOM - Document Object Model
+
+/*let n;
+n = document;
+n = document.all
+n = document.all [1]
+n = document.head
+n = document.title
+n = document.URL
+
+console.log(n);
+
+
+// Style
+
+let liste;
+
+/*liste = document.querySelector("li")
+liste = document.querySelector("li:nth-child(3)")
+
+liste.style.color = "blue"
+liste.style.backgroundColor = "red"*/
+
+/*liste = document.querySelectorAll("ul li")
+liste[1].style.color = "blue"
+
+console.log(liste);
+
+for (var i = 0; i < liste.length; i++){
+/*liste[i].style.backgroundColor = "green"
+liste[i].style.cssText = "color: #fff; background-color: blue"}*/
+
+/*========================================*/
+
+// GetElementById
+
+
+/*let n;
+
+n = document.getElementById('course-liste')
+n.textContent = "coucou !"
+n.style.color = "green" 
+
+console.log(n);*/
+
+let m;
+m = document.getElementById('liste');
+
+console.log(m);
+
+
